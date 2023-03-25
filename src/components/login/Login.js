23563f6e1cb969ba117ab734be73fcabeb506c8e
Rajat1ggc <Rajat1ggc@gmail.com>
2023-03-25 22:21:28 +0530

    made login lage by phone number

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,14 @@
 import { InputBox } from "components/common";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
 import firebaseConfig from "../../firebase/FirebaseConf";
 import { initializeApp } from "firebase/app";
+import "./login.css";
 
 function Login() {
   const [formData, setFormData] = useState({});
@@ -11,12 +16,20 @@ function Login() {
   const auth = getAuth();
 
   useEffect(() => {
-    window.recaptchaVerifier = new RecaptchaVerifier("recaptcha-container", { size: "invisible" }, auth);
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      { size: "invisible" },
+      auth
+    );
   }, []);
 
   const loginUser = () => {
     if (formData.phone.length == 10) {
-      signInWithPhoneNumber(auth, "+91" + formData.phone, window.recaptchaVerifier)
+      signInWithPhoneNumber(
+        auth,
+        "+91" + formData.phone,
+        window.recaptchaVerifier
+      )
         .then((confirmationResult) => {
           setResult(confirmationResult);
         })
@@ -40,7 +53,7 @@ function Login() {
 
   let data = {
     placeholder: "Phone Number",
-    type: "number",
+    type: "text",
     className: "form-control",
     value: formData.phone,
     onChange: (evt) => {
@@ -70,13 +83,29 @@ function Login() {
 
   return (
     <div className="container my-3">
-      <div>
-        <InputBox input={data} other={{ label: label }} />
-
-        <button type="submit" onClick={onclick} className="form-control btn btn-primary">
+      <form
+        class="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          onclick();
+        }}
+      >
+        <p class="form-title">Sign in to your account</p>
+        <div class="input-container">
+          <InputBox
+            placeholder="Enter your phone number"
+            input={data}
+            other={{ label: label }}
+          />
+        </div>
+        <button class="submit" type="submit" onClick={onclick}>
           {btnText}
         </button>
-      </div>
+        <p class="signup-link">
+          No account?
+          <div>Sign up</div>
+        </p>
+      </form>
     </div>
   );
 }
